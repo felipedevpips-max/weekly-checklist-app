ce8c45ce17618ef6ee070fd62468ea2a246a62c5
felipedevpips-max <felipedevpips@gmail.com>
2026-02-23 14:28:11 -0300

    aplicando estilização provisoria

--- frontend/src/types/task.ts
+++ frontend/src/types/task.ts
@@ -1,10 +1,11 @@
 export type Task = {
   id: number;
   title: string;
+  description?: string;
   status: "pending" | "in_progress" | "done";
   priority: "low" | "medium" | "high";
-  description?: string;   // DESCRIÇÃO
-  progress?: number;      // PROGRESSO
-  dueDate?: string;       // PRAZO
-  notify?: boolean;       // para futuras notificações
+  progress: number;
+  notify?: boolean;
+  createdAt: string;
+  dueDate?: string;
 };

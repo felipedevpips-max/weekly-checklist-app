f22ce79ff37e90e2cbabddcab378c440bb778b46
felipedevpips-max <felipedevpips@gmail.com>
2026-02-25 16:19:14 -0300

    aplicando hover

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,16 +6,20 @@ export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerContainer}>
+        <div>
+          <Logo className={styles.logo} />
+        </div>
 
-        <Logo className={styles.logo} />
-
-        <nav className={styles.nav}>
-          <Link to="/">Início</Link>
-          <Link to="/history">Histórico</Link>
-          <Link to="/about">Sobre</Link>
-          <button>Logout</button>
-        </nav>
-
+        <div className={styles.navContainer}>
+          <nav className={styles.nav}>
+            <Link to="/">Início</Link>
+            <Link to="/history">Histórico</Link>
+            <Link to="/about">Sobre</Link>
+          </nav>
+        </div>
+        <div className={styles.logoutContainer}>
+          <Link to="/logout">Logout</Link>
+        </div>
       </div>
     </header>
   );

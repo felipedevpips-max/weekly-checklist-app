431df3c3a1285c0e10fd515dedf197624bebd281
felipedevpips-max <felipedevpips@gmail.com>
2026-02-25 15:49:57 -0300

    estilizando header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import Logo from "../../assets/logo.svg?react";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <h2 className={styles.title}>Weekify</h2>
+      <div className={styles.headerContainer}>
+
+        <Logo className={styles.logo} />
 
         <nav className={styles.nav}>
           <Link to="/">Início</Link>
-
           <Link to="/history">Histórico</Link>
-
           <Link to="/about">Sobre</Link>
-
           <button>Logout</button>
         </nav>
+
       </div>
     </header>
   );

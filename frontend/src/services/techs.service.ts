2ae8eadf384d2a2c0ea3a08298dd5d3b9ec75a89
felipedevpips-max <felipedevpips@gmail.com>
2026-03-06 00:52:28 -0300

    aplicando techs

--- frontend/src/services/techs.service.ts
+++ frontend/src/services/techs.service.ts
@@ -2,6 +2,6 @@ import { api } from "../services/api";
 import type { Tech } from "../types/Tech";
 
 export async function getTechs(): Promise<Tech[]> {
-  const response = await api.get("/api/techs");
+  const response = await api.get("/techs");
   return response.data;
 }

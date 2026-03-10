e37e80d1fc7efb294af44700ee82bfe08cecde54
felipedevpips-max <felipedevpips@gmail.com>
2026-03-10 14:11:00 -0300

    fix

--- frontend/src/components/CreateTaskForm/CreateTaskForm.tsx
+++ frontend/src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { api } from "../../services/api";
-import styles from "./createtaskform.module.css";
+import styles from "./createTaskForm.module.css";
 import type { Task } from "../../types/task";
 
 type Props = {

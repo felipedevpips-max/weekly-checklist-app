546fbdfd43c22c86c98d75e272fca30ca591a4e3
felipedevpips-max <felipedevpips@gmail.com>
2026-03-11 17:24:08 -0300

    fix name

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        <p className={styles.logo}>Weekify</p>
+        <p className={styles.logo}>WeekTask</p>
 
         <p className={styles.description}>
           Sistema de gerenciamento semanal de tarefas para organizar metas e
@@ -18,7 +18,7 @@ export function Footer() {
         </p>
 
         <p className={styles.copy}>
-          © {year} Weekify — by FelipeCosta
+          © {year} WeekTask — by FelipeCosta
         </p>
       </div>
     </footer>
